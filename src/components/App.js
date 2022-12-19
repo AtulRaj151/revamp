2c6f9693ec5e5d0797bbe0b4e8b86faa4d1c30dc
AtulRaj151 <atulsid151@gmail.com>
2022-12-19 23:02:55 +0530

    add styled component material ui

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+import { Container } from './App.style';
+
+function App() {
+  return (
+    <Container className="App">
+      Hello
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/App.style.js
@@ -0,0 +1,8 @@
+
+import { styled } from '@mui/system';
+
+
+export const Container = styled.div`
+    color: black;
+    background-color: rebeccapurple;
+`;
