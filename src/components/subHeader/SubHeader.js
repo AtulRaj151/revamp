6b23f07ee42054d5bb89e5f43eba5be10d93656c
AtulRaj151 <atulsid151@gmail.com>
2022-12-22 15:13:53 +0530

    added subHeader breadcrumbs and subName

--- src/components/subHeader/SubHeader.js
+++ src/components/subHeader/SubHeader.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import { Wrapper } from './SubHeader.style';
+import { Wrapper, BreadCrumbsContainer, BreadCrumbsSubHeading } from './SubHeader.style';
+import { Breadcrumbs, Typography } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+import { PAGES_MAPPING } from '../../helper/routes';
 const SubHeader = () => {
+    const location = useLocation();
+    const [ breadcrumbs, setBreadCrumbs ] = useState({});
+    useEffect(()=> {
+       const path =  PAGES_MAPPING.filter((item)=> (item.path === location.pathname));
+        setBreadCrumbs(path[0])
+    },[location])
     return (
     <Wrapper data-testid="subHeader-container">
      <Box
@@ -12,10 +21,18 @@ const SubHeader = () => {
         backgroundColor: 'primary.main',
         '&:hover': {
           backgroundColor: 'primary.main',
-          opacity: [0.9, 0.8, 0.7],
+          opacity: [0.7, 0.8, 1],
         },
       })}
-    />
+    >
+       <BreadCrumbsContainer>
+        <Typography color="text.breadcrumb" sx={{ fontSize: 13}}>{breadcrumbs.name}</Typography>
+        <BreadCrumbsSubHeading>
+        {breadcrumbs.desc}
+        </BreadCrumbsSubHeading>
+        </BreadCrumbsContainer>
+
+    </Box>
     </Wrapper>
     )
 }
